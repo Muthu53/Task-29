24e2636f167550cc3fd959b3c5426a3a3556ff20
Muthu  R <106005632+Muthu53@users.noreply.github.com>
2022-07-27 06:11:22 +0530

    Create mentor.js

---
+++ mentor.js
@@ -0,0 +1,24 @@
+let mentors = [{
+        id: 1,
+        name: "Mentor1",
+        email: "mentor1@gmail.com",
+        subject: "Node",
+        company: "ABC"
+    },
+    {
+        id: 2,
+        name: "Mentor2",
+        email: "mentor2@gmail.com",
+        subject: "Javascript",
+        company: "XYZ"
+    },
+    {
+        id: 3,
+        name: "Mentor3",
+        email: "mentor3@gmail.com",
+        subject: "React",
+        company: "EFG"
+    }
+]
+
+module.exports = mentors;
